d2793d1515173e838a16d6c0b3c06c470991d927
John Margotti <jgotti1@optonline.net>
2023-02-10 12:31:49 -0500

    app.js tweak

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import EditPage from "./components/EditPage";
 
