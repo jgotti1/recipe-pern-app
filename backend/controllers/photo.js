376962592c9991952197ec55a25317ab4b5730e1
jgotti1 <jgotti1@optonline.net>
2024-02-18 10:10:22 -0500

    build commit

--- backend/controllers/photo.js
+++ backend/controllers/photo.js
@@ -21,19 +21,6 @@ const upload = multer({ storage: storage });
 
 router.use(cors());
 
-// router.put("/:recipeId", upload.single("photo"), async (req, res) => {
-//   const recipeId = req.params.recipeId;
-//   const photoName = req.file.originalname;
-
-//   try {
-//     await database.query("UPDATE recipes SET photo_name = $1 WHERE id = $2", [photoName, recipeId]);
-//     res.status(200).send("Photo name updated successfully");
-//   } catch (error) {
-//     console.error("Error updating photo name:", error);
-//     res.status(500).send("Error updating photo name");
-//   }
-// });
-
 
 router.put("/:recipeId", upload.single("photo"), async (req, res) => {
   const recipeId = req.params.recipeId;
