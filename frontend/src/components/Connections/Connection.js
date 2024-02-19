e5251f8fa51f147dd43451c174b5c74a5e705cbf
jgotti1 <jgotti1@optonline.net>
2024-02-19 14:22:43 -0500

    added .env info for firebase connection

--- frontend/src/components/Connections/Connection.js
+++ frontend/src/components/Connections/Connection.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVqeRkOJJId_fWCiESz2nKiXq8l09XXAE",
-  authDomain: "recipephotos-b5621.firebaseapp.com",
-  projectId: "recipephotos-b5621",
-  storageBucket: "recipephotos-b5621.appspot.com",
-  messagingSenderId: "562646961487",
-  appId: "1:562646961487:web:4572fe00d53158d583093a"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
